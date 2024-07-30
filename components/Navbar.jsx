5d050546589e1991f8b253fddf1ac39b0876df53
Abrar74774 <abrarshahriarhossain@gmail.com>
2024-07-30 22:18:49 +0300

    make navbar responsive (in progress)

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,25 +1,32 @@
 import styles from '@/styles/Navbar.module.css'
+import Hamburger from '@/public/hamburger-svgrepo-com.svg'
 import { gsap } from "gsap/dist/gsap";
 import { useGSAP } from "@gsap/react";
 
 import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+import { useEffect, useState } from 'react';
 
 
 gsap.registerPlugin(useGSAP, ScrollTrigger);
 export default function Navbar() {
+    const [open, setOpen] = useState(false)
+
     useGSAP(() => {
         gsap.set(`.${styles["nav-content"]}`, {
             opacity: 1
         })
-        gsap.from(`.${styles["nav-content"]} a`, {
-            opacity: 0,
-            y: 5,
-            stagger: 0.2,
-            ease: "power4.out",
-            duration: 1,
-            delay: 1.5
-        })
 
+        if (open) {
+            gsap.from(`.${styles["nav-content"]} a`, {
+                opacity: 0,
+                y: 5,
+                stagger: 0.2,
+                ease: "power4.out",
+                duration: 1,
+                delay: 1.5
+            })
+        }
+        
         gsap.from(`.${styles.navbar} .${styles["nav-background"]}`, {
             scrollTrigger: {
                 trigger: `#about`,
@@ -33,25 +40,58 @@ export default function Navbar() {
             duration: 0.5
         })
     })
+
+    // useGSAP(() => {
+    //     if (open) {
+    //         gsap.from(`.${styles["nav-content"]} a`, {
+    //             opacity: 0,
+    //             y: 5,
+    //             stagger: 0.2,
+    //             ease: "power4.out",
+    //             duration: 1
+    //         })
+    //     } 
+    // }, [open])
+
+    useEffect(() => {
+        if (window.screen.width > 600) {
+            setOpen(true)
+        }
+    }, [])
+    
+    const handleBurgerClick = (e) => {
+        setOpen(!open)
+    };
+
+
+    const handleLinkClick = (e) => {
+        setOpen(false)
+    };
+
+
+
     return (
         <nav className={styles.navbar}>
-            <div className={`${styles["nav-background"]}`}></div>
-            <div className={`${styles["nav-content"]}`}>
-                <a href="#about">
+            <div className={`${styles["nav-background"]} not-mobile`}></div>
+            <div className={`${styles["nav-content"]}`} style={!open ? { visibility: 'hidden' } : {}}>
+                <a href="#about" onClick={handleLinkClick}>
                     About
                 </a>
-                <a href="#portfolio">
+                <a href="#portfolio" onClick={handleLinkClick}>
                     Works
                 </a>
-                <a href="#contact">
+                <a href="#contact" onClick={handleLinkClick}>
                     Contact
                 </a>
-                <a href="https://github.com/Abrar74774" target="_blank">
+                <a onClick={handleLinkClick} href="https://github.com/Abrar74774" target="_blank">
                     <i className="fab fa-github"></i>
                 </a>
-                <a href="https://www.linkedin.com/in/abrar-hossain-069505196/" target="_blank">
+                <a onClick={handleLinkClick} href="https://www.linkedin.com/in/abrar-hossain-069505196/" target="_blank">
                     <i className="fab fa-linkedin"></i>
                 </a>
+            </div>
+            <div className={`${styles['hamburger-container']} mobile`} onClick={handleBurgerClick}>
+                <Hamburger />
             </div>
         </nav>
     )
