7b4a26c2c9309d50e67b770602e29926630fc98e
Abrar74774 <abrarshahriarhossain@gmail.com>
2024-08-04 00:45:17 +0300

    Make bottom to top animation for all lines in Hero

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,6 +1,6 @@
 import styles from '@/styles/Hero.module.css'
 import Navbar from './Navbar.jsx'
-import { useRef, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import gsap from 'gsap/dist/gsap'
 import { useGSAP } from '@gsap/react'
 
@@ -9,7 +9,10 @@ gsap.registerPlugin(useGSAP)
 const mouseMoveDivisor = 60
 
 export default function Hero() {
-    const [ coords, setCoords ] = useState({x: 0, y:0})
+    const [coords, setCoords] = useState({ x: 0, y: 0 })
+    const [isMobile, setIsMobile] = useState(false)
+    const [isMobileSet, setIsMobileSet] = useState(false)
+
     const helloContainer = useRef(null)
     const handleMouseMove = e => {
         setCoords({
@@ -19,20 +22,30 @@ export default function Hero() {
     }
 
     useGSAP(() => {
-        gsap.set(`.${styles.hello}`, {visibility: 'visible'})
-        gsap.from(`.${styles.hello} .frame div`, {
-            yPercent: 110,
-            ease: "power4.out",
-            stagger: 0.5,
-            duration: 2,
-            delay: 0.3
-        })
-        
-    }, { scope: helloContainer})
+        if (isMobileSet) {
+            gsap.set(`.${styles.hello}`, { visibility: 'visible' })
+            gsap.from(`.${styles.hello} .frame div`, {
+                yPercent: 110,
+                ease: "power4.out",
+                stagger: 0.2,
+                duration: 2,
+                delay: 0.3
+            })
+        }
+
+    }, { scope: helloContainer, dependencies: [isMobileSet] })
+
+    useEffect(() => {
+        if (window.screen.width < 600) {
+            setIsMobile(true)
+        }
+    }, [])
+
+    useEffect(() => { setIsMobileSet(true)}, [isMobile])
 
     return (
         <header className={styles.home}>
-            <div className={styles.homepic} style={{ transform: `translateX(${coords.x}px) translateY(${coords.y}px)`}}>
+            <div className={styles.homepic} style={{ transform: `translateX(${coords.x}px) translateY(${coords.y}px)` }}>
                 <video autoPlay muted loop>
                     <source src="vid/blue.webm" type="video/webm" />
                     <source src="vid/blue.mp4" type="video/mp4" />
@@ -41,18 +54,45 @@ export default function Hero() {
             <div className={`${styles["home-content"]}`} onMouseMove={handleMouseMove}>
                 <Navbar />
                 <div ref={helloContainer} className={`${styles['hello-container']}`}>
-                    <h1 className={styles.hello}>
-                        <div className="frame">
-                            <div>
-                                Hello. I&apos;m <span className={styles.glow}>Abrar&nbsp;Shahriyar</span><br />
+                    {isMobile ?
+                        <h1 className={styles.hello}>
+                            <div className="frame">
+                                <div>
+                                    Hello. I&apos;m
+                                </div>
                             </div>
-                        </div>
-                        <div className="frame">
-                            <div>
-                                I&apos;m A Full Stack Developer.
+                            <div className="frame">
+                                <div>
+                                <span className={styles.glow}>Abrar&nbsp;Shahriyar</span>
+                                </div>
                             </div>
-                        </div>
-                    </h1>
+                            <div className="frame">
+                                <div>
+                                    I&apos;m A Full Stack 
+                                </div>
+                            </div>
+                            <div className="frame">
+                                <div>
+                                    Developer.
+                                </div>
+                            </div>
+                        </h1>
+                        :
+                        <h1 className={styles.hello}>
+                            <div className="frame">
+                                <div>
+                                    Hello. I&apos;m <span className={styles.glow}>Abrar&nbsp;Shahriyar</span><br />
+                                </div>
+                            </div>
+                            <div className="frame">
+                                <div>
+                                    I&apos;m A Full Stack Developer.
+                                </div>
+                            </div>
+                        </h1>
+
+
+                    }
                 </div>
             </div>
         </header>
