2f96ee627fcc32d7d3d88ab4c7587e8da037c223
Abrar74774 <abrarshahriarhossain@gmail.com>
2025-02-03 04:56:44 +0300

    Update footer styling slightly

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,8 +3,13 @@ export default function Footer() {
     return (
         <footer className={styles.footer}>
             <div className="container">
-                Designed and built by Abrar Shahriyar. <a href="https://github.com/Abrar74774/portfolio-v2"
-                    target="_blank">View Source Code</a>
+                <div>
+                    Designed and built by Abrar Shahriyar.
+                </div>
+                <div>
+                    <a href="https://github.com/Abrar74774/portfolio-v2"
+                        target="_blank">View Source Code</a>
+                </div>
             </div>
         </footer>
     )
